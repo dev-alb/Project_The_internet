47d19752b30ea9cf8b098d1a10328bac8fd0ad9c
vinialb <vini.alb13@gmail.com>
2023-08-10 20:11:36 -0300

    project

---
+++ index.js
@@ -0,0 +1,20 @@
+function showParagraph(id) {
+    let count = 1
+    let paragraph = document.getElementById(`paragraph-${count}`)
+
+    while (!!paragraph) {
+
+        if (count === id) { 
+            if (paragraph.className === 'showing') {
+                paragraph.className = 'hidden'
+            } else {
+                paragraph.className = 'showing'
+            }
+        } else {
+            paragraph.className = 'hidden'
+        }
+
+        count ++
+        paragraph = document.getElementById(`paragraph-${count}`)
+    }
+}
